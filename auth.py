381e573a3e7575eddc267e732ef2b5501832fac3
Deepakraj23344555 <deepak.raj@msfa.christuniversity.in>
2025-06-25 22:45:05 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -7,7 +7,7 @@ def make_hash(password):
 def create_users_table():
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY, password TEXT)')
     conn.commit()
     conn.close()
 
